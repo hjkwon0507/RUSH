d7ddbe1e52944b8afc4bc0c8b3764cecc8ad9cac
hyejin-kwon <7applejink@gmail.com>
2022-01-11 13:50:43 +0900

    모각코 복습

---
+++ Youtube/src/App.js
@@ -0,0 +1,11 @@
+import Hello from './Hello.js';
+
+function App() {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ Youtube/src/Hello.js
@@ -0,0 +1,5 @@
+function Hello(){
+  return <div>Hello, React!</div>;
+}
+
+export default Hello;
